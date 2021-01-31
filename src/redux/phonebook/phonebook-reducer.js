c48ed5d446ab75d9a3c208e5f9ea34bb9d8e96bb
TOR968 <tor968@yahoo.com>
2021-01-31 17:30:05 +0200

    Update phonebook-reducer.js

--- src/redux/phonebook/phonebook-reducer.js
+++ src/redux/phonebook/phonebook-reducer.js
@@ -1,20 +1,73 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
-import { addContact, deleteContact, filterContact } from './phonebook-action';
+import {
+  filterContact,
+  fetchContactRequest,
+  fetchContactSuccess,
+  fetchContactError,
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  removeContactRequest,
+  removeContactSuccess,
+  removeContactError,
+} from './phonebook-action';
+
+const fetchContact = (_, { payload }) => {
+  return [...payload];
+};
+
+const addContact = (state, { payload }) => {
+  return [...state, payload];
+};
+
+const removeContact = (state, { payload }) => {
+  return state.filter(({ id }) => id !== payload);
+};
 
 const items = createReducer([], {
-  [addContact]: (state, { payload }) => [...state, payload],
-  [deleteContact]: (state, { payload }) =>
-    state.filter(({ id }) => id !== payload),
+  [fetchContactSuccess]: fetchContact,
+  [addContactSuccess]: addContact,
+  [removeContactSuccess]: removeContact,
 });
 
 const filter = createReducer('', {
-  [filterContact]: (_, { payload }) => payload,
+  [filterContact.type]: (_, { payload }) => payload,
+});
+
+const loading = createReducer(false, {
+  [fetchContactRequest]: () => true,
+  [fetchContactSuccess]: () => false,
+  [fetchContactError]: () => false,
+
+  [addContactRequest]: () => true,
+  [addContactSuccess]: () => false,
+  [addContactError]: () => false,
+
+  [removeContactRequest]: () => true,
+  [removeContactSuccess]: () => false,
+  [removeContactError]: () => false,
+});
+
+const error = createReducer(false, {
+  [fetchContactRequest]: () => '',
+  [fetchContactSuccess]: () => '',
+  [fetchContactError]: (_, { payload }) => payload,
+
+  [addContactRequest]: () => '',
+  [addContactSuccess]: () => '',
+  [addContactError]: (_, { payload }) => payload,
+
+  [removeContactRequest]: () => '',
+  [removeContactSuccess]: () => '',
+  [removeContactError]: (_, { payload }) => payload,
 });
 
 const phonebookReducer = combineReducers({
   items,
   filter,
+  loading,
+  error,
 });
 
 export default phonebookReducer;
