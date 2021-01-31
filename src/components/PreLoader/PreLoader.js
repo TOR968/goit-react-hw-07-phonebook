4705350c2c5dd90bab699cda210ede834e9bf947
TOR968 <tor968@yahoo.com>
2021-01-31 17:29:40 +0200

    Update PreLoader.js

--- src/components/PreLoader/PreLoader.js
+++ src/components/PreLoader/PreLoader.js
@@ -1,14 +1,7 @@
-import Loader from 'react-loader-spinner';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-
-import LoaderContainer from './PreLoaderStyles';
+import { Loader } from './PreLoaderStyles';
 
 const PreLoader = () => {
-  return (
-    <LoaderContainer>
-      <Loader type="Puff" color="#00BFFF" height={100} width={100} />
-    </LoaderContainer>
-  );
+  return <Loader />;
 };
 
 export default PreLoader;
